2dbe0c808da3df605328a02850102579b06f56bc
Rafael <Rafael@LAPTOP-SV57APN8>
2021-02-26 14:30:17 -0300

    criado projeto

---
+++ CadCaminhoes/CadCaminhoes/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CadCaminhoes.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
